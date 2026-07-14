327041d191f4b1ef27ebd6761a3725bfb036396d
Sujith K <sujithkaruvanchery@gmail.com>
2026-07-14 15:30:00 +0530

    Learn JavaScript variables and scope

--- app.js
+++ app.js
@@ -1,1 +1,154 @@
-console.log("Hello, JavaScript!");
+// let → Keyword
+// name → Variable name
+// "Sujith" → Value
+
+// let age = 22;
+
+// console.log(age);
+
+// age = 23;
+
+// console.log(age);
+
+// const country = "India";
+
+// console.log(country);
+
+// country = "USA";
+
+// console.log(country);
+
+// var city = "Tirur";
+
+// console.log(city);
+
+// city = "Kottakkal"
+
+// console.log(city)
+
+
+// Variable Naming Rules
+
+// let name = "Sujith";
+
+// console.log(name);
+
+// let firstName = "Sujith";
+
+// console.log(firstName);
+
+// let first_name = "Sujith";
+
+// console.log(first_name);
+
+// let age25 = 25;
+
+// console.log(age25);
+
+// let $price = 100;
+
+// console.log($price);
+
+// let _count = 5;
+
+// console.log(_count);
+
+// let 1name = "Sujith";
+
+// console.log(1name);
+
+// let first-name = "Sujith";
+
+// console.log(first-name);
+
+// let let = 10;
+
+// console.log(let);
+
+// let a = 10;
+// let b = 20;
+
+// a = 30;
+
+// console.log(a);
+// console.log(b);
+
+// function greet() {
+//     let name = "Sujith";
+//     console.log(name);
+// }
+
+// greet();
+
+// function greet() {
+//     let name = "Sujith";
+// }
+
+// console.log(name);
+
+// greet()
+
+// function test() {
+//     var age = 24;
+//     console.log(age);
+// }
+
+// test();
+
+// console.log(age);
+
+// function greet() {
+//     const name = "Sujith";
+//     console.log(name);
+// }
+
+// greet();
+
+// const name = "Sujith";
+
+// function greet() {
+//     console.log(name);
+// }
+
+// greet();
+
+// console.log(name);
+
+// if (true) {
+//     let name = "Sujith";
+//     console.log(name);
+// }
+
+// console.log(name);
+
+
+
+// Function Scope
+
+// function functionScopeExample() {
+//     if (true) {
+//         var functionScopedVar = "I am accessible anywhere in this function!";
+//     }
+
+//     console.log(functionScopedVar);
+// }
+
+// functionScopeExample();
+
+// console.log(functionScopedVar);
+
+// Block Scope
+
+function blockScopeExample() {
+    if (true) {
+        let blockScopedLet = "I am trapped in this block!";
+        const blockScopedConst = "Me too!";
+        console.log(blockScopedLet);
+        console.log(blockScopedConst)
+    }
+
+    console.log(blockScopedLet);
+    console.log(blockScopedConst)
+}
+
+blockScopeExample()
